75a872f51dfbfa1b2f8a6a2d433d1460fc95db0c
Prakash Pun <prakash_pun@persistent.co.in>
2019-06-17 14:39:56 +0530

    Added methods to Add data and clear al fields

--- app.js
+++ app.js
@@ -13,7 +13,26 @@ function UI() { }
 
 // Prototype function for UI class to add book to list;
 UI.prototype.addBookToList = function(book) {
-    
+    const list = document.getElementById('book-list');
+
+    // Create table row element
+    const row = document.createElement('tr');
+    // Insert columns
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a> </td>
+    `;
+
+    list.appendChild(row);
+}
+
+// Clear UI fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
 }
 
 // Event listeners
@@ -33,6 +52,8 @@ document.getElementById('book-form').addEventListener('submit',
         // Add book to list
         ui.addBookToList(book)    
 
+        // Clear UI fields
+        ui.clearFields();
         
         e.preventDefault();
     });
